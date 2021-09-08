402f4bfe1722ee1b62e5d5561b40444131f7c10b
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-09-08 14:21:59 -0700

    update db.js for heroku

--- db/db.js
+++ db/db.js
@@ -1,8 +1,6 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const db = new Sequelize("messenger", process.env.LOCAL_USERNAME, process.env.LOCAL_PASSWORD, {
-  host: "127.0.0.1",
-  dialect: "postgres"
-})
+const db = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = db;
