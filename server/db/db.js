58927b98656d7c0dafbac37d16f58bb2ae68423d
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-09-08 22:33:26 -0700

    more sequelize config

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,9 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const db = new Sequelize(process.env.DATABASE_URL)
+const db = new Sequelize(process.env.DATABASE_URL, process.env.DATABASE_USER, {
+    host: process.env.DATABASE_HOST,
+    dialect: "postgres"
+})
 
 module.exports = db;
