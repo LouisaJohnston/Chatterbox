62507897b29cfcd4a0246af70e5807721a02894a
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-09-10 17:01:00 -0700

    working npm start

--- server/db/db.js
+++ server/db/db.js
@@ -1,26 +1,13 @@
 const Sequelize = require("sequelize");
-require("dotenv").config();
 
-// const db = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     protocol: 'postgres',
-//     port: process.env.PORT,
-//     host: process.env.DATABASE_HOST,
-//     logging: true,
-// })
-
-// module.exports = db;
-
-// const db = new Sequelize(process.env.DATABASE_URL, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-//     dialect: 'postgres',
-//     host: process.env.DATABASE_HOST
-// })
-
-const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    host: process.env.DATABASE_HOST,
-    logging: console.log()
-})
-
-module.exports = db;
+if (process.env.DATABASE_URL) {
+    const db = new Sequelize(process.env.DATABASE_URL)
+    module.exports = db;
+} else {
+    const db = new Sequelize("messenger", process.env.LOCAL_USERNAME, process.env.LOCAL_PASSWORD, {
+        host: "127.0.0.1",
+        dialect: "postgres"
+    })
+    
+    module.exports = db;
+}
