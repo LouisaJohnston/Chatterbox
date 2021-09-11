eb812de882259f48912bbb2ad30940235c5514b9
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-09-10 19:04:13 -0700

    work on react build folder

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -7,6 +7,7 @@ router.use("/users", require("./users"));
 router.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
+  console.log(error)
   next(error);
 });
 
